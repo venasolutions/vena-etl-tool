d5c323a78dd4c1d0197bd6e2ca3cabf1aa0abeb1
Patrick Nadeau <pnadeau@venasolutions.com>
2014-06-13 15:28:49 -0400

    Moved the HTTP client code for performing the upload to ETLClient where it more properly belongs.  Bumped version number to 1.9

--- src/main/java/org/vena/etltool/ETLClient.java
+++ src/main/java/org/vena/etltool/ETLClient.java
@@ -1,22 +1,36 @@
 package org.vena.etltool;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map.Entry;
 
 import javax.ws.rs.core.MediaType;
 
 import org.vena.api.customer.authentication.LoginResult;
+import org.vena.api.etl.ETLFile;
 import org.vena.api.etl.ETLJob;
+import org.vena.api.etl.ETLMetadata;
 import org.vena.etltool.entities.CreateModelRequestDTO;
 import org.vena.etltool.entities.ModelResponseDTO;
+import org.vena.etltool.util.TwoTuple;
 import org.vena.id.Id;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.multipart.FormDataBodyPart;
+import com.sun.jersey.multipart.FormDataMultiPart;
+import com.sun.jersey.multipart.impl.MultiPartWriter;
 
 public class ETLClient {
 	protected int port = 8080;
@@ -34,6 +48,121 @@ public class ETLClient {
 	public ETLClient() {
 	}
 	 
+	public void uploadETL(ETLMetadata metadata)
+	{
+		try {
+
+			ClientConfig jerseyClientConfig = new DefaultClientConfig();
+			jerseyClientConfig.getClasses().add(MultiPartWriter.class);
+			
+			Client client = Client.create(jerseyClientConfig);
+
+			client.addFilter(new HTTPBasicAuthFilter(apiUser, apiKey));
+			
+			String uri = buildURI();
+
+			System.out.println("Calling "+uri);
+
+			WebResource webResource = client.resource(uri);
+
+			webResource.accept("application/json");
+
+			FormDataMultiPart form = new FormDataMultiPart();
+
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			byte[] metadataBytes = objectMapper.writeValueAsBytes(metadata);
+
+			FormDataBodyPart metadataPart = new FormDataBodyPart("metadata",new ByteArrayInputStream(metadataBytes), MediaType.APPLICATION_JSON_TYPE);
+			form.bodyPart(metadataPart);
+
+			
+			for(Entry<String, ETLFile> entry :  metadata.getFiles().entrySet()) {
+				String key = entry.getKey();
+				ETLFile etlFile = entry.getValue();
+				
+				etlFile.setMimePart(key);
+				
+				FormDataBodyPart filePart = new FormDataBodyPart(key, new FileInputStream(etlFile.getFilename()), MediaType.APPLICATION_OCTET_STREAM_TYPE);
+				form.bodyPart(filePart);
+			}
+			
+			ClientResponse response = webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE).post(ClientResponse.class, form);
+			
+			System.out.println(">>> " + response);
+
+			switch( response.getStatus()) {
+			
+			case 200:
+				ETLJob output = response.getEntity(ETLJob.class);
+
+				System.out.println("Job submitted. Your ETL Job Id is "+output.getId());
+				
+				break;
+			case 401:
+				System.err.println("Access denied.  Check your credentials and try again.");
+				System.exit(2);
+				break;
+			case 403:
+				System.err.println("Permission denied. Login was successful, but your user does not have permission to perform an ETL upload.");
+				System.exit(2);
+				break;
+			default:
+				System.err.println("Error"+response);
+				System.exit(4);
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+	}
+	
+	private  String buildURI()
+	{
+		StringBuilder urlBuf = new StringBuilder();
+		List<TwoTuple<String, String>> parameters = new ArrayList<>();
+		
+		urlBuf.append(protocol).append("://");
+		urlBuf.append(host).append(":");
+		urlBuf.append(port);
+		
+		String resource;
+		
+		if( !validationRequested ) {
+			resource = "/api/etl/upload";
+		}
+		else {
+			resource = "/api/etl/validate";
+		}
+		
+		urlBuf.append(resource);
+		
+		if( templateId != null )
+			parameters.add(new TwoTuple<String, String>("templateId", templateId));
+		 
+		Iterator<TwoTuple<String, String>> it = parameters.iterator();
+		
+		StringBuilder parameterBuf = new StringBuilder();
+		
+		if(it.hasNext())
+			parameterBuf.append("?");
+		
+		while(it.hasNext()) {
+			TwoTuple<String, String> parameter = it.next();
+			
+			parameterBuf.append(parameter.getO1()).append("=").append(parameter.getO2());
+			
+			if(it.hasNext())
+				parameterBuf.append("&");
+		}
+
+		urlBuf.append(parameterBuf);
+		
+		return urlBuf.toString();
+	}
+	
 	public void login()
 	{
 		Client client = Client.create();
--- src/main/java/org/vena/etltool/Main.java
+++ src/main/java/org/vena/etltool/Main.java
@@ -1,16 +1,8 @@
 package org.vena.etltool;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -25,25 +17,11 @@ import org.vena.api.etl.ETLFile.Type;
 import org.vena.api.etl.ETLJob;
 import org.vena.api.etl.ETLMetadata;
 import org.vena.etltool.entities.ModelResponseDTO;
-import org.vena.etltool.util.TwoTuple;
 import org.vena.id.Id;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
-import com.sun.jersey.multipart.FormDataBodyPart;
-import com.sun.jersey.multipart.FormDataMultiPart;
-import com.sun.jersey.multipart.impl.MultiPartWriter;
 
 public class Main {
 	private static final String EXAMPLE_COMMANDLINE = "etl-tool --host=localhost --port=8080 --apiUser=1.1 --apiKey=4d87c176227045de9628fb5f010a7b40 --file=model.csv;hierarchy";
 
-	
-	
 	/**
 	 * @param args
 	 * @throws UnsupportedEncodingException 
@@ -55,114 +33,9 @@ public class Main {
 
 		ETLMetadata metadata = parseCmdlineArgs(args, etlClient);
 
-		try {
-
-			ClientConfig jerseyClientConfig = new DefaultClientConfig();
-			jerseyClientConfig.getClasses().add(MultiPartWriter.class);
-			
-			Client client = Client.create(jerseyClientConfig);
-
-			client.addFilter(new HTTPBasicAuthFilter(etlClient.apiUser, etlClient.apiKey));
-			
-			String uri = buildURI(etlClient);
-
-			System.out.println("Calling "+uri);
-
-			WebResource webResource = client.resource(uri);
-
-			webResource.accept("application/json");
-
-			FormDataMultiPart form = new FormDataMultiPart();
-
-			ObjectMapper objectMapper = new ObjectMapper();
-
-			byte[] metadataBytes = objectMapper.writeValueAsBytes(metadata);
-
-			FormDataBodyPart metadataPart = new FormDataBodyPart("metadata",new ByteArrayInputStream(metadataBytes), MediaType.APPLICATION_JSON_TYPE);
-			form.bodyPart(metadataPart);
-
-			
-			for(Entry<String, ETLFile> entry :  metadata.getFiles().entrySet()) {
-				String key = entry.getKey();
-				ETLFile etlFile = entry.getValue();
-				
-				etlFile.setMimePart(key);
-				
-				FormDataBodyPart filePart = new FormDataBodyPart(key, new FileInputStream(etlFile.getFilename()), MediaType.APPLICATION_OCTET_STREAM_TYPE);
-				form.bodyPart(filePart);
-			}
-			
-			ClientResponse response = webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE).post(ClientResponse.class, form);
-			
-			System.out.println(">>> " + response);
-
-			switch( response.getStatus()) {
-			
-			case 200:
-				ETLJob output = response.getEntity(ETLJob.class);
-
-				System.out.println("Job submitted. Your ETL Job Id is "+output.getId());
-				
-				break;
-			case 401:
-				System.err.println("Access denied.  Check your credentials and try again.");
-				System.exit(2);
-				break;
-			default:
-				System.err.println("Error"+response);
-				System.exit(3);
-			}
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-
-		}
+		etlClient.uploadETL(metadata);
 	}
 
-	private static String buildURI(ETLClient etlClient)
-	{
-		StringBuilder urlBuf = new StringBuilder();
-		List<TwoTuple<String, String>> parameters = new ArrayList<>();
-		
-		urlBuf.append(etlClient.protocol).append("://");
-		urlBuf.append(etlClient.host).append(":");
-		urlBuf.append(etlClient.port);
-		
-		String resource;
-		
-		if( !etlClient.validationRequested ) {
-			resource = "/api/etl/upload";
-		}
-		else {
-			resource = "/api/etl/validate";
-		}
-		
-		urlBuf.append(resource);
-		
-		if( etlClient.templateId != null )
-			parameters.add(new TwoTuple<String, String>("templateId", etlClient.templateId));
-		 
-		Iterator<TwoTuple<String, String>> it = parameters.iterator();
-		
-		StringBuilder parameterBuf = new StringBuilder();
-		
-		if(it.hasNext())
-			parameterBuf.append("?");
-		
-		while(it.hasNext()) {
-			TwoTuple<String, String> parameter = it.next();
-			
-			parameterBuf.append(parameter.getO1()).append("=").append(parameter.getO2());
-			
-			if(it.hasNext())
-				parameterBuf.append("&");
-		}
-
-		urlBuf.append(parameterBuf);
-		
-		return urlBuf.toString();
-	}
 	
 	@SuppressWarnings("static-access")
 	private  static ETLMetadata parseCmdlineArgs(String[] args, ETLClient etlClient) throws UnsupportedEncodingException {
