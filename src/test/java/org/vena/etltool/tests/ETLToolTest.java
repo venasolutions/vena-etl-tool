093a950d8de5a728bda9406dc8022e48d5a358a6
ssoliman <ssoliman@venasolutions.com>
2017-03-06 11:41:25 -0500

    Correcting indentation and formatting, removing whitespace

--- src/test/java/org/vena/etltool/tests/ETLToolTest.java
+++ src/test/java/org/vena/etltool/tests/ETLToolTest.java
@@ -18,44 +18,44 @@ import org.vena.etltool.entities.ModelResponseDTO;
 
 
 public abstract class ETLToolTest {
-	
+
 	protected Id modelId;
-	
+
 	protected static final SecurityManager originalManager = System.getSecurityManager();
-	
+
 	protected final PrintStream originalSystemErr = System.err;
 	protected final PrintStream originalSystemOut = System.out;
-	
+
 	protected final ByteArrayOutputStream out = new ByteArrayOutputStream();
 	protected final ByteArrayOutputStream err = new ByteArrayOutputStream();
-	
-    @BeforeClass
-    public static void setNoExitSecurityManager() {
-    	System.setSecurityManager(new NoExitSecurityManager());
-    }
-    
-    @AfterClass
-    public static void tearDown() {
-    	System.setSecurityManager(originalManager);
-    }
-    
+
+	@BeforeClass
+	public static void setNoExitSecurityManager() {
+		System.setSecurityManager(new NoExitSecurityManager());
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		System.setSecurityManager(originalManager);
+	}
+
 	@Before
 	public void setUpStreams() {
-	    System.setOut(new PrintStream(out));
-	    System.setErr(new PrintStream(err));
+		System.setOut(new PrintStream(out));
+		System.setErr(new PrintStream(err));
 	}
 
 	@After
 	public void cleanUpStreams() {
-	    System.setOut(originalSystemErr);
-	    System.setErr(originalSystemOut);
+		System.setOut(originalSystemErr);
+		System.setErr(originalSystemOut);
 	}
-	
+
 	protected String[] buildCommandArgs() {
 		String[] commandArgs = {"--username=admin@vena.io", "--password=vena", "--modelName=myModel"};
 		return commandArgs;
 	}
-	
+
 	protected String[] buildCommand(String[] cmdArgs) {
 		String[] authArgs = buildCommandArgs();
 		String[] args = Arrays.copyOf(authArgs, authArgs.length + cmdArgs.length);
@@ -66,46 +66,48 @@ public abstract class ETLToolTest {
 		
 		return args;
 	}
-	
+
 	protected ETLClient mockETLClient() {
 		ETLClient mockedETLClient = mock(ETLClient.class);
-		
+
 		ModelResponseDTO searchResults = new ModelResponseDTO();
 		modelId = uniqueId();
 		searchResults.setName("myModel");
 		searchResults.setId(modelId);
-		
+
 		when(mockedETLClient.lookupModel("myModel")).thenReturn(searchResults);
-		
+
 		return mockedETLClient;
 	}
-	
-    @SuppressWarnings("serial")
-	protected static class ExitException extends SecurityException {
-        public final int status;
-        public ExitException(int status) {
-            this.status = status;
-        }
-    }
 
-    private static class NoExitSecurityManager extends SecurityManager {
-        @Override
-        public void checkPermission(Permission perm) {}
-        
-        @Override
-        public void checkPermission(Permission perm, Object context) {}
-        
-        @Override
-        public void checkExit(int status) {
-            super.checkExit(status);
-            throw new ExitException(status);
-        }
-    }
-    
-    protected Id uniqueId() {
-    	long seed = System.currentTimeMillis();
-    	Random random = new Random(seed);
-    	return new Id(seed + random.nextInt(10000));
-    }
-    
+	@SuppressWarnings("serial")
+	protected static class ExitException extends SecurityException {
+		public final int status;
+
+		public ExitException(int status) {
+			this.status = status;
+		}
+	}
+
+	private static class NoExitSecurityManager extends SecurityManager {
+		@Override
+		public void checkPermission(Permission perm) {
+		}
+
+		@Override
+		public void checkPermission(Permission perm, Object context) {
+		}
+
+		@Override
+		public void checkExit(int status) {
+			super.checkExit(status);
+			throw new ExitException(status);
+		}
+	}
+
+	protected Id uniqueId() {
+		long seed = System.currentTimeMillis();
+		Random random = new Random(seed);
+		return new Id(seed + random.nextInt(10000));
+	}
 }
